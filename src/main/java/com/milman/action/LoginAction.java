db10ec3f9a29ff1d8ebb88a83b807ff8ce4af2c9
Assylkhan <assylkhan.rakhatov@mail.ru>
2015-11-12 23:57:37 +0600

    some changes

--- src/main/java/com/milman/action/LoginAction.java
+++ src/main/java/com/milman/action/LoginAction.java
@@ -3,13 +3,14 @@ package com.milman.action;
 import com.milman.dataSource.UserDao;
 import com.milman.entity.User;
 import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.dispatcher.DefaultActionSupport;
 
-public class LoginAction extends ActionSupport {
+public class LoginAction extends DefaultActionSupport {
     private UserDao userDao;
     private User user;
 
-    public void setUserDAO(UserDao userDAO) {
-        this.userDao = userDao;
+    public void setUserDao(UserDao userDao) {
+        this.userDao = this.userDao;
     }
 
     public void setUser(User user) {
